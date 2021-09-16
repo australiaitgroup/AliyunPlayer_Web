06ebc71fb2cb1259051135680554d9b1c988b7e4
Terence MA <terence.ma.lsu@gmail.com>
2021-09-16 10:42:28 +1000

    Rate componet  can receive props and save data on change in localstorage, key:'aliplayer-playback-speed'

--- customComponents/src/components/RateComponent/index.js
+++ customComponents/src/components/RateComponent/index.js
@@ -1,66 +1,103 @@
-import rateHtml from './index.html'
-import './index.scss'
-import { parseDom } from 'utils'
+import rateHtml from "./index.html";
+import "./index.scss";
+import { parseDom } from "utils";
 
 /**
  * 倍速播放组件
  */
 export default class RateComponent {
-  /**
-   * @constructor 倍速播放组件构造函数
-   */
-  constructor () {
-    this.html = parseDom(rateHtml)
-  }
+	/**
+	 * @constructor 倍速播放组件构造函数
+	 * @param currentSpeedString {@String Playbackspeed default '1.0'}
+	 */
+	constructor(currentSpeedString = "1.0") {
+		this.html = parseDom(rateHtml);
+		this.currentSpeed = currentSpeedString;
+		this.availableSpeeds = ["0.5", "1.0", "1.25", "1.5", "2.0"];
+	}
 
-  createEl (el) {
-    let eleControlbar = el.querySelector('.prism-controlbar')
-    eleControlbar.appendChild(this.html)
-  }
+	createEl(el) {
+		let eleControlbar = el.querySelector(".prism-controlbar");
+		eleControlbar.appendChild(this.html);
+	}
 
-  ready (player, e) {
-    let currentRateEle = this.html.querySelector('.current-rate')
-    let rateListEle = this.html.querySelector('.rate-list')
-    let timeId = null
+	created(player) {
+		if (this.availableSpeeds.includes(this.currentSpeed)) {
+			let currentRateEle = this.html.querySelector(".current-rate");
+			currentRateEle.innerText = currentSpeed + "x";
+			let rateListEleArray = this.html.querySelectorAll(".rate-list > li");
+			currentRateEle.innerText = this.currentSpeed + "x";
+			[...rateListEleArray].map((item) => {
+				if (item.dataset.rate === rate) {
+					item.classList.add("current");
+				}
+				if (item.dataset.rate !== rate) {
+					item.classList.remove("current");
+				}
+				return;
+			});
+			player.setSpeed(parseFloat(this.currentSpeed));
+		} else {
+			this.currentSpeed = "1.0";
+			let currentRateEle = this.html.querySelector(".current-rate");
+			currentRateEle.innerText = currentSpeed + "x";
+			let rateListEleArray = this.html.querySelectorAll(".rate-list > li");
+			currentRateEle.innerText = this.currentSpeed + "x";
+			[...rateListEleArray].map((item) => {
+				if (item.dataset.rate === rate) {
+					item.classList.add("current");
+				}
+				if (item.dataset.rate !== rate) {
+					item.classList.remove("current");
+				}
+				return;
+			});
+			player.setSpeed(1);
+		}
+	}
 
-    // 隐藏设置里面的倍速播放
-    let settingRate = document.querySelector('.prism-setting-item.prism-setting-speed')
-    if (settingRate) {
-     settingRate.classList.add('player-hidden')
-    }
+	ready(player, e) {
+		let currentRateEle = this.html.querySelector(".current-rate");
+		let rateListEle = this.html.querySelector(".rate-list");
+		let timeId = null;
 
-    currentRateEle.onclick = () => {
-      rateListEle.style.display = 'block'
-    }
-    currentRateEle.onmouseleave = () =>{
-      timeId = setTimeout(() => {
-        rateListEle.style.display = 'none'
-      }, 100);
-    }
+		// 隐藏设置里面的倍速播放
+		let settingRate = document.querySelector(".prism-setting-item.prism-setting-speed");
+		if (settingRate) {
+			settingRate.classList.add("player-hidden");
+		}
 
-    rateListEle.onmouseenter = () => {
-      clearTimeout(timeId)
-    }
-    rateListEle.onmouseleave = () => {
-      rateListEle.style.display = 'none'
-    }
+		currentRateEle.onclick = () => {
+			rateListEle.style.display = "block";
+		};
+		currentRateEle.onmouseleave = () => {
+			timeId = setTimeout(() => {
+				rateListEle.style.display = "none";
+			}, 100);
+		};
 
-    rateListEle.onclick = ({target}) => {
-      let rate = target.dataset.rate
-      if (rate) {
-        player.setSpeed(rate)
-        if (target.className !== 'current') {
-          let currentEle = rateListEle.querySelector('.current')
-          if (currentEle) {
-            currentEle.className = ''
-          }
-          target.className = 'current'
-        }
-        rateListEle.style.display = 'none'
-        currentRateEle.innerText = rate + 'x'
+		rateListEle.onmouseenter = () => {
+			clearTimeout(timeId);
+		};
+		rateListEle.onmouseleave = () => {
+			rateListEle.style.display = "none";
+		};
 
-      }
-    }
-  }
-
+		rateListEle.onclick = ({ target }) => {
+			let rate = target.dataset.rate;
+			window.localStorage.setItem("aliplayer-playback-speed", `${rate}`);
+			if (rate) {
+				player.setSpeed(rate);
+				if (target.className !== "current") {
+					let currentEle = rateListEle.querySelector(".current");
+					if (currentEle) {
+						currentEle.className = "";
+					}
+					target.className = "current";
+				}
+				rateListEle.style.display = "none";
+				currentRateEle.innerText = rate + "x";
+			}
+		};
+	}
 }
